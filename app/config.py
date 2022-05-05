e1525d91ddac5233dfcc1e32c2a2582c80f00001
r00t254 <root254.mary@gmail.com>
2022-05-05 10:42:17 +0300

    add secret key

--- app/config.py
+++ app/config.py
@@ -6,6 +6,8 @@ class Config:
     '''
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
     CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
     # NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export NEWS_API_KEY='623074f2452f46be92620a6b97439557'
+export SECRET_KEY = '123'
 
 python3.8  manage.py server
