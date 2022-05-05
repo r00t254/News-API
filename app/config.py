023104e551a3bf59473a580a52c0083748b25156
r00t254 <root254.mary@gmail.com>
2022-05-05 08:58:57 +0300

    Modify requirements file

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
     CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
     # NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-  export NEWS_API_KEY='623074f2452f46be92620a6b97439557'
+export NEWS_API_KEY='623074f2452f46be92620a6b97439557'
 
 python3.8  manage.py server
